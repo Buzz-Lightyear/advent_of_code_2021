b8fa0e3f53aaf912eeccf11d2de91d23a7195343
Srinivas Muthu <smuthu@linkedin.com>
2021-12-27 16:40:36 -0800

    Completed day 6

--- code/day5/day5.py
+++ code/day5/day5.py
@@ -5,9 +5,76 @@
 """
 
 def main():
+    grid = {}
     with open("input.txt", "r") as f:
         lines = [[[int(z) for z in y.split(',')] for y in x.rstrip().split(' -> ')] for x in f.readlines()]
-        print(lines[0])
+        for (a, b), (c, d) in lines:
+            if a == c:
+                low = min(b, d)
+                high = max(b, d)
+                for y in range(low, high+1):
+                    if (a,y) in grid:
+                        grid[(a,y)] += 1
+                    else:
+                        grid[(a,y)] = 1
+            elif b == d:
+                low = min(a, c)
+                high = max(a, c)
+                for x in range(low, high+1):
+                    if (x,b) in grid:
+                        grid[(x,b)] += 1
+                    else:
+                        grid[(x,b)] = 1
+            elif abs(a-c) == abs(b-d):
+                # diagonal
+                if a < c and b < d:
+                    x = a
+                    y = b
+                    while x <= c and y <= d:
+                        if (x,y) in grid:
+                            grid[(x,y)] += 1
+                        else:
+                            grid[(x,y)] = 1
+                        x += 1
+                        y += 1
+                elif a > c and b < d:
+                    x = a
+                    y = b
+                    while x >= c and y <= d:
+                        if (x,y) in grid:
+                            grid[(x,y)] += 1
+                        else:
+                            grid[(x,y)] = 1
+                        x -= 1
+                        y += 1
+                elif a < c and b > d:
+                    x = a
+                    y = b
+                    while x <= c and y >= d:
+                        if (x,y) in grid:
+                            grid[(x,y)] += 1
+                        else:
+                            grid[(x,y)] = 1
+                        x += 1
+                        y -= 1
+                else:
+                    x = a
+                    y = b
+                    while x >= c and y >= d:
+                        if (x,y) in grid:
+                            grid[(x,y)] += 1
+                        else:
+                            grid[(x,y)] = 1
+                        x -= 1
+                        y -= 1
+        
+        sum = 0
+        for (i, j) in grid:
+            if grid[(i,j)] > 1:
+                sum += 1
+        
+        print(sum)
+
 
 if __name__ == '__main__':
     main()
